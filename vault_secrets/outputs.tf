dcf07e73d57e2c263373133a6275bfdcca289e7e
Alisdair McDiarmid <alisdair@users.noreply.github.com>
2024-01-19 15:09:47 -0500

    Reintroduce nonsensitive hack
    This may not be fixed after all.

--- vault_secrets/outputs.tf
+++ vault_secrets/outputs.tf
@@ -1,5 +1,7 @@
 output "github_token" {
   description = "The static GitHub token stored securely in HCP Vault Secrets, fetched via OIDC authentication"
 
-  value = data.hcp_vault_secrets_secret.github_token.secret_value
+  # TODO: Stacks fail to serialize a sensitive value, so we temporarily nonsensitive() the value for
+  # now. Without, it will fail with ".token: value has marks, so it cannot be serialized"
+  value = nonsensitive(data.hcp_vault_secrets_secret.github_token.secret_value)
 }
