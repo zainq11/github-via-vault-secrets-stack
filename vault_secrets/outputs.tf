5954222a7a10ded90788c9e50a0c76c740d26fda
Alisdair McDiarmid <alisdair@users.noreply.github.com>
2024-01-19 14:34:01 -0500

    Remove nonsensitive hack
    This Terraform bug is now fixed.

--- vault_secrets/outputs.tf
+++ vault_secrets/outputs.tf
@@ -1,7 +1,5 @@
 output "github_token" {
   description = "The static GitHub token stored securely in HCP Vault Secrets, fetched via OIDC authentication"
 
-  # TODO: Stacks fail to serialize a sensitive value, so we temporarily nonsensitive() the value for
-  # now. Without, it will fail with ".token: value has marks, so it cannot be serialized"
-  value = nonsensitive(data.hcp_vault_secrets_secret.github_token.secret_value)
+  value = data.hcp_vault_secrets_secret.github_token.secret_value
 }
